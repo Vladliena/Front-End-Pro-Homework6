1af6fdd008633e5de8124397868f0e4944646ebd
vladliena <sotnikovavladlena@gmail.com>
2022-12-05 15:34:08 +0200

    last commit

--- js/scripts.js
+++ js/scripts.js
@@ -4,23 +4,24 @@ let city = prompt('please add the city', 'Tel Aviv, Odesa, Paris, Venice, Istanb
 switch (city) {
     case 'TEL AVIV':
         coordinates = '32.085300;34.781769';
+        alert (`Coordinates for ${city}: ${coordinates}`)
         break;
     case 'ODESA':
         coordinates = '46.482525;30.723309';
+        alert (`Coordinates for ${city}: ${coordinates}`)
         break;
     case 'PARIS':
         coordinates = '48.856613;2.352222';
+        alert (`Coordinates for ${city}: ${coordinates}`)
         break;
     case 'VENICE':
         coordinates = '45.440845;12.315515';
+        alert (`Coordinates for ${city}: ${coordinates}`)
         break;
     case 'ISTANBUL':
         coordinates = '41.015137;28.979530';
+        alert (`Coordinates for ${city}: ${coordinates}`)
         break;
     default:
-        alert('we dont have such coordinates, sorry');
+        alert('we dont have such coordinates, restart page');
 }
-
-let finalString = `Coordinates for ${city}: ${coordinates}`;
-
-alert(`${finalString}`)
